4adcf06fb51ef4f51b70dbfed6a42ee1731d14d1
Igor Julliano <igor.julliano2@gmail.com>
2023-03-08 19:45:17 -0300

    feat: move game server to unique folder
    In order to bring both server and client to the same repository, game server
    code will now be on server and game client will be in it proper folder soon

---
+++ server/src/index.ts
@@ -0,0 +1,41 @@
+import * as net from 'net';
+
+const server: net.Server = net.createServer()
+const activeConnections = new Map<String, net.Socket>()
+
+server.on("connection", (socket: net.Socket) => {
+   activeConnections.set(`${socket.remoteAddress}:${socket.remotePort}`, socket)
+   
+   console.log(`[Server] Current connections:`)
+   for (const connection of activeConnections.keys()){
+      console.log(connection);
+   }
+   
+   socket.on("connect", () => {
+      console.log(`[Server] Cliente que acaba de conectar: ${socket.remoteAddress}:${socket.remotePort}`);
+   })
+
+   socket.on("ready", () => { console.log("Client ready") })
+   socket.on("data", (data) => { 
+      console.log(`[Client] ${data}`)
+      socket.write("Escutado. Beleza, vlw 👍")
+   })
+
+   socket.on('close', (hadError) => {
+      console.log(`[Server] Conexão com ${socket.remoteAddress} finalizada` + (hadError ? " com erros" : ""))
+      activeConnections.delete(`${socket.remoteAddress}:${socket.remotePort}`)
+   })
+})
+
+server.on('end', () => {
+   console.log('Server ended');
+});
+
+server.on("close", () => {
+   console.log("Servidor fechado")
+})
+
+const port = 3000
+server.listen(port, () => {
+   console.log(`Servidor inicializado na porta ${port}`);
+});
