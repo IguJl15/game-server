0dcace92378120e987db62816952717419d8b9b2
Kaike Dias <kaikedias98@gmail.com>
2023-03-08 23:28:46 -0300

    Fit: Add representation of queries and mutations

--- server/src/api/Queries.ts
+++ server/src/api/Queries.ts
@@ -1,23 +1,4 @@
-interface CreateGameParameters {
-    boardId: String
-    playerName?: String
-}
-abstract class CreateGameQueryValidator {
-    
-    static validate(jsonQuery: Object): String | null {
-            
-        if(!jsonQuery.hasOwnProperty("playerName")) return "propriedade 'playerName' é obrigatória"
-        if(!jsonQuery.hasOwnProperty("symbol")) return "propriedade 'symbol' é obrigatória"
-        
-        if(typeof jsonQuery )
-
-        return null
-    }
-}
-
-abstract class StringParameterValidator {
-    static validate(text: String) {
-        const trimmedValue = text.trim()
-
-    }
+interface GameState {
+    boardId: string
+    playerId: string
 }
