6d1878d3b566454323a1d8afd129ffc4a3f488ac
Igor Julliano <igor.julliano2@gmail.com>
2023-03-10 22:55:55 -0300

    refactor: change constructor and more aiming readability

--- server/src/game/entities/Board.ts
+++ server/src/game/entities/Board.ts
@@ -1,52 +1,75 @@
+import { AlreadyMarkedError } from "../Errors";
+import Cloneable from "../utils/Cloneable";
 import Player from "./Player";
-import Square from "./Square"
+import Square from "./Square";
 
-export
-class Board {
-    private squares: Square[] = Array(9)
-    public currentPlayer: Player;
+export class Board extends Cloneable {
+    boardId: string;
 
-    private player2!: Player;
-    constructor(
-        public id: String,
+    player1: Player;
+    player2!: Player;
+    currentPlayer: Player;
+    winner: Player | null
 
-        private readonly player1: Player,
+    status: "waiting" | "in game" | "finished"
+
+    private squares: Square[]
+
+    public get isFilled(): boolean {
+        return !this.squares.some(x => x.value == null)
+    }
+
+
+    public constructor(
+        boardId: string,
+        player1: Player
     ) {
+        super()
+        this.boardId = boardId
+        this.player1 = player1
         this.currentPlayer = player1
-        this.squares.fill(Square.blank())
+        this.winner = null
+
+        this.status = "waiting"
+        this.squares = Array.from({ length: 9 }, () => Square.blank())
     }
 
-    public player2Joined(player2: Player) {
-        this.player2 = player2
-    }
-
+    /**
+     * ### **ATTENTION**: this method automatically swap current player to the next one
+     * 
+     * Set the square in the given index with the current player symbol.]
+     * @param position where player will mark
+     * @throws AlreadyMarkedError
+     */
     public mark(position: number) {
         let square = this.squares[position]
-        
-        if(square.value != null) throw new AlreadyMarkedError();
-        
+
+        if (square.value != null) throw new AlreadyMarkedError();
+
         square.value = this.currentPlayer.value
 
-        this.checkForWinner()
-        
-        this.currentPlayer = this.currentPlayer == this.player1 
-                                                ? this.player2 
-                                                : this.player1
+        this.checkBoard()
+
+        this.currentPlayer = this.currentPlayer.id == this.player1.id
+            ? this.player2
+            : this.player1
     }
-    
-    checkForWinner() {
+
+    checkBoard() {
         const victoryPaths = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
             [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6],
         ]
-        
-        if (victoryPaths.some(arrangement => 
-                arrangement.every(position => 
-                    this.squares[position].value == this.currentPlayer.value
-                )
-            )) {
-            console.log("Current ganhou")
+
+        if (victoryPaths.some(
+            arrangement => arrangement.every(position => this.squares[position].value == this.currentPlayer.value)
+        )) {
+            this.setWinner(this.currentPlayer)
         }
     }
 
+    setWinner(player: Player) {
+        this.winner = player;
+        this.status = "finished"
+    }
 }
