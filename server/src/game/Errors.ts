1abd8258bf85bf7c64a8dacb9792ff465716e8d9
Igor Julliano <igor.julliano2@gmail.com>
2023-03-15 12:19:22 -0300

    feat: add application errors

--- server/src/game/Errors.ts
+++ server/src/game/Errors.ts
@@ -1,5 +1,32 @@
 class AlreadyMarkedError extends Error {
-    constructor() {
-        super("Posição já preenchida")
-    }
+    constructor() { super("Posição já preenchida") }
+}
+
+class BoardNotFoundError extends Error {
+    constructor() { super("Jogo não encontrado") }
+}
+
+class FullBoardError extends Error {
+    constructor() { super("Jogo cheio") }
+}
+
+class CanNotMarkPositionError extends Error {
+    constructor() { super("Você não pode jogar nesse momento") }
+}
+
+class GameNotRunningError extends Error {
+    constructor() { super("O jogo não está em execução no momento") }
+}
+
+class UnknownOperationError extends Error {
+    constructor() { super("Operação não reconhecida") }
+}
+
+export {
+    AlreadyMarkedError,
+    BoardNotFoundError,
+    CanNotMarkPositionError,
+    FullBoardError,
+    GameNotRunningError,
+    UnknownOperationError,
 }
