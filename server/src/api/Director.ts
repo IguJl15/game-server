9cf8f4fe328fb4a8de8eb412a97dc972be4a7ba0
Igor Julliano <igor.julliano2@gmail.com>
2023-03-15 12:21:47 -0300

    feat: add director to orchestrate server requests

--- server/src/api/Director.ts
+++ server/src/api/Director.ts
@@ -1,5 +1,89 @@
-class Director {
-    constructor() {}
+import SocketServer from "..";
+import { UnknownOperationError } from "../game/Errors";
+import CreateGameCommand from "../game/commands/CreateGameCommand";
+import GetGameStateCommand from "../game/commands/GetGameStateCommand";
+import JoinGameCommand from "../game/commands/JoinGameCommand";
+import MarkPositionCommand from "../game/commands/MarkSquareCommand";
+import QuitGameCommand from "../game/commands/QuitGameCommand";
+import { Board } from "../game/entities/Board";
+import ArrayBoardRepo, { BoardRepository } from "../game/repositories/BoardRepository";
+import Notifier, { SocketNotifier } from "../game/utils/Notifier";
+import { CloseGame, CreateGameParameters, JoinGame, MarkPosition } from "./Mutations";
 
-    direct()
+export default
+    class Director {
+
+    static playerNotifier: Notifier = new SocketNotifier();
+
+    static boardRepository: BoardRepository = new ArrayBoardRepo();
+
+    static getGameStateCommand = new GetGameStateCommand(this.boardRepository);
+    static createGameCommand = new CreateGameCommand(this.boardRepository, this.getGameStateCommand);
+    static joinGameCommand = new JoinGameCommand(this.getGameStateCommand, this.boardRepository, this.playerNotifier);
+    static markPositionCommand = new MarkPositionCommand(this.boardRepository, this.getGameStateCommand, this.playerNotifier)
+    static quitGameCommand = new QuitGameCommand(this.boardRepository, this.playerNotifier)
+
+    static direct(action: string, request: any) {
+        let requestPlayerId: string
+        let gameState: { GameState: Board } | null
+
+        switch (action) {
+            case "CreateGame":
+                gameState = this.createGame(request["CreateGame"] as CreateGameParameters);
+                requestPlayerId = gameState.GameState.player1.id!;
+                break;
+            case "JoinGame":
+                gameState = this.joinGame(request["JoinGame"] as JoinGame);
+                requestPlayerId = gameState.GameState.player2.id!;
+                break;
+            case "MarkPosition":
+                requestPlayerId = (request["MarkPosition"] as MarkPosition).playerId
+                gameState = this.markPosition(request["MarkPosition"] as MarkPosition);
+                break;
+            case "CloseGame":
+                this.closeGame(request as CloseGame);
+                return
+            default:
+                throw new UnknownOperationError();
+        }
+
+        if (gameState != null) {
+            SocketServer.activeConnections.set(requestPlayerId, SocketServer.currentConnection!);
+        }
+
+        return gameState;
+    }
+
+    static createGame(params: CreateGameParameters) {
+        const newBoard = this.createGameCommand.execute(params);
+
+        return {
+            "GameState": newBoard
+        }
+    }
+
+    static joinGame(params: JoinGame) {
+        const updatedBoard = this.joinGameCommand.execute(params);
+
+        const gameState = {
+            "GameState": updatedBoard
+        }
+
+        return gameState
+    }
+
+    static markPosition(params: MarkPosition) {
+        const updatedBoard = this.markPositionCommand.execute(params)
+
+        const gameState = {
+            "GameState": updatedBoard
+        }
+
+        return gameState
+    }
+    
+    static closeGame(params: CloseGame) {
+        this.quitGameCommand.execute(params)
+    }
 }
+
