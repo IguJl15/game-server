eabc00821a0a787224750c4ef38774e89e367561
KelsonF <kfeduardo2003@gmail.com>
2023-03-16 01:13:53 -0300

    feat: add new redirect statement

--- server/src/api/Director.ts
+++ server/src/api/Director.ts
@@ -8,7 +8,10 @@ import QuitGameCommand from "../game/commands/QuitGameCommand";
 import { Board } from "../game/entities/Board";
 import ArrayBoardRepo, { BoardRepository } from "../game/repositories/BoardRepository";
 import Notifier, { SocketNotifier } from "../game/utils/Notifier";
-import { CloseGame, CreateGameParameters, JoinGame, MarkPosition } from "./Mutations";
+import { CloseGame, CreateGameParameters, IUserCredentials, IUserRegister, JoinGame, MarkPosition } from "./Mutations";
+import LoginUserCommand from "../auth/commands/LoginUserCommand";
+import UserRepository, { IUserRepository } from "../auth/repositories/UserRepositorie";
+import RegisterUserCommand from "../auth/commands/RegisterUserCommand";
 
 export default
     class Director {
@@ -17,13 +20,18 @@ export default
 
     static boardRepository: BoardRepository = new ArrayBoardRepo();
 
+    static userRepository: IUserRepository = new UserRepository();
+
     static getGameStateCommand = new GetGameStateCommand(this.boardRepository);
     static createGameCommand = new CreateGameCommand(this.boardRepository, this.getGameStateCommand);
     static joinGameCommand = new JoinGameCommand(this.getGameStateCommand, this.boardRepository, this.playerNotifier);
     static markPositionCommand = new MarkPositionCommand(this.boardRepository, this.getGameStateCommand, this.playerNotifier)
     static quitGameCommand = new QuitGameCommand(this.boardRepository, this.playerNotifier)
+    static loginUserCommand = new LoginUserCommand(this.userRepository)
+    static registerUserCommand = new RegisterUserCommand(this.userRepository)
 
     static direct(action: string, request: any) {
+        let registerPlayer: string
         let requestPlayerId: string
         let gameState: { GameState: Board } | null
 
@@ -40,6 +48,10 @@ export default
                 requestPlayerId = (request["MarkPosition"] as MarkPosition).playerId
                 gameState = this.markPosition(request["MarkPosition"] as MarkPosition);
                 break;
+            case "Register":
+                return this.createUser(request["Register"] as IUserRegister)
+            case "Register":
+                return this.verifyUser(request["Login"] as IUserCredentials)
             case "CloseGame":
                 this.closeGame(request as CloseGame);
                 return
@@ -84,6 +96,20 @@ export default
     
     static closeGame(params: CloseGame) {
         this.quitGameCommand.execute(params)
+    }
+
+    static createUser(params:IUserRegister) {
+        const createNewUser = this.registerUserCommand.execute(params)
+        return {
+            "User" : createNewUser
+        }
+    }
+
+    static verifyUser(params: IUserCredentials) {
+        const loggedUser = this.loginUserCommand.execute(params)
+        return {
+            "User" : loggedUser
+        }
     }
 }
 
