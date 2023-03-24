9577dd2b40bc1877f991dbe299d2b9d90169c3b4
Igor Julliano <igor.julliano2@gmail.com>
2023-03-23 23:40:29 -0300

    feat: update models due api changes

--- client/lib/models/player.dart
+++ client/lib/models/player.dart
@@ -1,20 +1,17 @@
 class Player {
-  String? id;
   String name;
   int points;
   bool isCurrent;
 
   Player({
-    required this.id,
     required this.name,
     required this.points,
     required this.isCurrent,
   });
 
-  factory Player.empty(String name) => Player(id: '', name: name, isCurrent: false, points: 0);
+  factory Player.empty(String name) => Player(name: name, isCurrent: false, points: 0);
 
   factory Player.fromJson(Map<String, dynamic> json, bool isCurrent) => Player(
-        id: json['id'],
         name: json['name'],
         points: int.tryParse(json['points'] ?? '') ?? 0,
         isCurrent: isCurrent,
